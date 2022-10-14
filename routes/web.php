02fe2ec29154b856ba55348296efe7171f64578d
Lucas Narcizo Neto <lucasnarcizo99@hotmail.com>
2022-10-14 16:20:17 -0300

    projeto finalizado

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\ClientController;
+
+
+Route::get('/', function(){
+    return redirect('/client');
 });
+
+Auth::routes(['verify' => true]);
+
+//Home Page
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Client Routes
+Route::get('/client', 'ClientController@index');
+Route::post('/client/create', 'ClientController@store');
+Route::get('/client/edit/{id}', 'ClientController@edit');
+Route::put('/client/edit/{id}', 'ClientController@update');
+Route::delete('/client/delete/{id}', 'ClientController@destroy');
